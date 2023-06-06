de9428b6bca5902af85b580e0485545e74b17a56
Johannes Waigel <johannes@waigel.me>
2023-06-06 19:59:34 +0200

    Disable social media card

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -67,7 +67,7 @@ const config = {
       respectPrefersColorScheme: false,
     },
       // Replace with your project's social card
-      image: 'img/docusaurus-social-card.jpg',
+      //image: 'img/docusaurus-social-card.jpg',
       navbar: {
         title: 'sicherheit.wtf',
         // logo: {
